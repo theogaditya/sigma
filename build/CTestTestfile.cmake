52c79b3a8047245e5a5a9324f9bf2ba63e742d3d
aditya <adityahota99@gmail.com>
2025-12-29 21:30:13 +0530

    Fix CI: remove build/ from git, remove Linux ARM64 build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aditya/code/projects/lang-cus
-# Build directory: /home/aditya/code/projects/lang-cus/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SigmaTests "/home/aditya/code/projects/lang-cus/build/bin/sigma_tests")
-set_tests_properties(SigmaTests PROPERTIES  _BACKTRACE_TRIPLES "/home/aditya/code/projects/lang-cus/CMakeLists.txt;205;add_test;/home/aditya/code/projects/lang-cus/CMakeLists.txt;0;")
